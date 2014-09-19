069daec95732fe44a5af9d704c4f33716acf82b2
unknown <dw29044@WDXCND41706GM0.jdnet.deere.com>
2014-09-19 16:53:18 -0500

    tweaking charts

--- ZeroMq_Python/application/static/monitor.js
+++ ZeroMq_Python/application/static/monitor.js
@@ -1,7 +1,7 @@
 (function() {
 	var queueDepths = [],
 		consumerCounts = [],
-		MAX_POINTS = 300;
+		MAX_POINTS = 100;
 	
 	function getDataPoint(){
 		$.get('/currentqueuedepth', function(data) {
@@ -15,30 +15,24 @@
 		});
 		
 		$.get('/currentconsumercount', function(data) {
-			queueDepths.push(data);
+			consumerCounts.push(data);
 			
-			if(queueDepths.length > MAX_POINTS)
-				queueDepths.shift(); // pop one of the old points off;
+			if(consumerCounts.length > MAX_POINTS)
+				consumerCounts.shift(); // pop one of the old points off;
 			
 			updateConsumerSummary(data);
-			updateGraph(data);
+			updateGraph();
 		});
 	}
 	
 	function updateQueueSummary(data) {
-		var newSummary = "The Queue is currently empty.";
-		if(data) {
-			newSummary = "The current queue depth is: <b>" + data + "</b>";
-		}
+		newSummary = "The current queue depth is: <b>" + data + "</b>";
 		
 		$('#queueSummary').html(newSummary);
 	}
 	
 	function updateConsumerSummary(data) {
-		var newSummary = "There are currently no Consumers.";
-		if(data) {
-			newSummary = "The current consumer count is: <b>" + data + "</b>";
-		}
+		newSummary = "The current consumer count is: <b>" + data + "</b>";
 		
 		$('#consumerSummary').html(newSummary);
 	}
@@ -54,9 +48,9 @@
 		}
 	
 	
-		var chartUrl = "https://chart.googleapis.com/chart?cht=lc&chs=600x400&chxt=x,y";
+		var chartUrl = "https://chart.googleapis.com/chart?cht=lc&chs=600x400&chxt=x,y&chco=FF0000,0000FF";
 		chartUrl += "&chxr=1,0," + maxPoint + "|0,0," + MAX_POINTS;
-		chartUrl += "&chco=FF0000, 0000FF";
+		chartUrl += "&chco=FF0000,0000FF";
 		chartUrl += "&chd=t:";
 		chartUrl += queueDepths.join(',');
 		chartUrl += "|";
