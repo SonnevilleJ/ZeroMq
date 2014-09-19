222ef9f0890d6afd5f5e18b5ca11f35d5b817ccc
unknown <dw29044@WDXCND41706GM0.jdnet.deere.com>
2014-09-19 16:20:24 -0500

    WIP

--- ZeroMq_Python/application/static/monitor.js
+++ ZeroMq_Python/application/static/monitor.js
@@ -1,49 +1,77 @@
 (function() {
-	var dataPoints = [];
+	var queueDepths = [],
+		consumerCounts = [],
+		MAX_POINTS = 300;
 	
 	function getDataPoint(){
 		$.get('/currentqueuedepth', function(data) {
-			dataPoints.push(data);
+			queueDepths.push(data);
 			
-			if(dataPoints.length > 300)
-				dataPoints.shift(); // pop one of the old points off;
+			if(queueDepths.length > MAX_POINTS)
+				queueDepths.shift(); // pop one of the old points off;
 			
-			updateSummary(data);
+			updateQueueSummary(data);
+			updateGraph();
+		});
+		
+		$.get('/currentconsumercount', function(data) {
+			queueDepths.push(data);
+			
+			if(queueDepths.length > MAX_POINTS)
+				queueDepths.shift(); // pop one of the old points off;
+			
+			updateConsumerSummary(data);
 			updateGraph(data);
 		});
 	}
 	
-	function updateSummary(data) {
+	function updateQueueSummary(data) {
 		var newSummary = "The Queue is currently empty.";
 		if(data) {
 			newSummary = "The current queue depth is: <b>" + data + "</b>";
 		}
 		
-		$('#summary').html(newSummary);
+		$('#queueSummary').html(newSummary);
+	}
+	
+	function updateConsumerSummary(data) {
+		var newSummary = "There are currently no Consumers.";
+		if(data) {
+			newSummary = "The current consumer count is: <b>" + data + "</b>";
+		}
+		
+		$('#consumerSummary').html(newSummary);
 	}
 	
 	var isGraphReady = false;
 	function updateGraph() {
 		var maxPoint = 5;
 		
-		for(var i = 0, ii = dataPoints.length; i < ii; i++) {
-			var point = dataPoints[i];
+		for(var i = 0, ii = queueDepths.length; i < ii; i++) {
+			var point = queueDepths[i];
 			if(point > maxPoint)
 				maxPoint = point;
 		}
 	
 	
 		var chartUrl = "https://chart.googleapis.com/chart?cht=lc&chs=600x400&chxt=x,y";
-		chartUrl += "&chxr=1,0," + maxPoint + "|0,0," + dataPoints.length;
+		chartUrl += "&chxr=1,0," + maxPoint + "|0,0," + MAX_POINTS;
+		chartUrl += "&chco=FF0000, 0000FF";
 		chartUrl += "&chd=t:";
-		chartUrl += dataPoints.join(',');
+		chartUrl += queueDepths.join(',');
+		chartUrl += "|";
+		chartUrl += consumerCounts.join(',');
 		
 		$('#chart').attr('src', chartUrl);
 	}
 
 	function start() {
+		for(var i = 0; i < MAX_POINTS; i++) {
+			queueDepths[i] = 0;
+			consumerCounts[i] = 0;
+		}
 		getDataPoint();
-		setInterval(getDataPoint, 2000);
+		setInterval(getDataPoint, 1000);
 	}
 
 	$(start);
